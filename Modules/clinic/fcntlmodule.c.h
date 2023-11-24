python/cpython

gh-96694: add F_GETLK wrapper in fcntl module

Modules/clinic/fcntlmodule.c.h
@@ -249,4 +249,82 @@ skip_optional:
 exit:
     return return_value;
 }
-/*[clinic end generated code: output=1db859412172dd53 input=a9049054013a1b77]*/
+
+PyDoc_STRVAR(fcntl_getlk__doc__,
+"getlk($module, fd, cmd, len=0, start=0, whence=0, /)\n"
+"--\n"
+"\n"
+"A wrapper around the fcntl(F_GETLK) locking call.\n"
+"\n"
+"`fd` is the file descriptor of the file whose lock status to get.  `cmd`\n"
+"is one of the following values:\n"
+"\n"
+"    LOCK_SH - check for conflicting exclusive lock\n"
+"    LOCK_EX - check for conflicting shared or exclusive lock\n"
+"\n"
+"Note that the returned information identifies a lock that would conflict with\n"
+"acquiring the type of lock specified in `cmd`.  Calling with LOCK_SH therefore\n"
+"only returns information on a conflicting LOCK_EX, while calling with LOCK_EX\n"
+"returns information on either LOCK_SH or LOCK_EX locks.\n"
+"\n"
+"The remaining parameters are as with `lockf`.\n"
+"\n"
+"Returns a tuple of (pid, cmd, len, start, whence) if a conflicting lock is\n"
+"found.  If more than one lock conflicts, platform code chooses one.  There\n"
+"may not be an associated process, in which case pid will be -1.\n"
+"\n"
+"If no other lock would conflict with the request, this function returns None.\n"
+"\n"
+"The information returned by this call may already be outdated by the time it\n"
+"returns.  Additionally, only locks acquired using `lockf` (or `fcntl(F_SETLK)`)\n"
+"are considered by this call.");
+
+#define FCNTL_GETLK_METHODDEF    \
+    {"getlk", _PyCFunction_CAST(fcntl_getlk), METH_FASTCALL, fcntl_getlk__doc__},
+
+static PyObject *
+fcntl_getlk_impl(PyObject *module, int fd, int code, PyObject *lenobj,
+                 PyObject *startobj, int whence);
+
+static PyObject *
+fcntl_getlk(PyObject *module, PyObject *const *args, Py_ssize_t nargs)
+{
+    PyObject *return_value = NULL;
+    int fd;
+    int code;
+    PyObject *lenobj = NULL;
+    PyObject *startobj = NULL;
+    int whence = 0;
+
+    if (!_PyArg_CheckPositional("getlk", nargs, 2, 5)) {
+        goto exit;
+    }
+    if (!_PyLong_FileDescriptor_Converter(args[0], &fd)) {
+        goto exit;
+    }
+    code = _PyLong_AsInt(args[1]);
+    if (code == -1 && PyErr_Occurred()) {
+        goto exit;
+    }
+    if (nargs < 3) {
+        goto skip_optional;
+    }
+    lenobj = args[2];
+    if (nargs < 4) {
+        goto skip_optional;
+    }
+    startobj = args[3];
+    if (nargs < 5) {
+        goto skip_optional;
+    }
+    whence = _PyLong_AsInt(args[4]);
+    if (whence == -1 && PyErr_Occurred()) {
+        goto exit;
+    }
+skip_optional:
+    return_value = fcntl_getlk_impl(module, fd, code, lenobj, startobj, whence);
+
+exit:
+    return return_value;
+}
+/*[clinic end generated code: output=12ef85c3a62f502b input=a9049054013a1b77]*/

hakeem-amsagguine/cpython-v2

Modules/clinic/fcntlmodule.c.h
@@ -246,4 +246,82 @@ skip_optional:
 exit:
     return return_value;
 }
-/*[clinic end generated code: output=732e33ba92042031 input=a9049054013a1b77]*/
+
+PyDoc_STRVAR(fcntl_getlk__doc__,
+"getlk($module, fd, cmd, len=0, start=0, whence=0, /)\n"
+"--\n"
+"\n"
+"A wrapper around the fcntl(F_GETLK) locking call.\n"
+"\n"
+"`fd` is the file descriptor of the file whose lock status to get.  `cmd`\n"
+"is one of the following values:\n"
+"\n"
+"    LOCK_SH - check for conflicting exclusive lock\n"
+"    LOCK_EX - check for conflicting shared or exclusive lock\n"
+"\n"
+"Note that the returned information identifies a lock that would conflict with\n"
+"acquiring the type of lock specified in `cmd`.  Calling with LOCK_SH therefore\n"
+"only returns information on a conflicting LOCK_EX, while calling with LOCK_EX\n"
+"returns information on either LOCK_SH or LOCK_EX locks.\n"
+"\n"
+"The remaining parameters are as with `lockf`.\n"
+"\n"
+"Returns a tuple of (pid, cmd, len, start, whence) if a conflicting lock is\n"
+"found.  If more than one lock conflicts, platform code chooses one.  There\n"
+"may not be an associated process, in which case pid will be -1.\n"
+"\n"
+"If no other lock would conflict with the request, this function returns None.\n"
+"\n"
+"The information returned by this call may already be outdated by the time it\n"
+"returns.  Additionally, only locks acquired using `lockf` (or `fcntl(F_SETLK)`)\n"
+"are considered by this call.");
+
+#define FCNTL_GETLK_METHODDEF    \
+    {"getlk", _PyCFunction_CAST(fcntl_getlk), METH_FASTCALL, fcntl_getlk__doc__},
+
+static PyObject *
+fcntl_getlk_impl(PyObject *module, int fd, int code, PyObject *lenobj,
+                 PyObject *startobj, int whence);
+
+static PyObject *
+fcntl_getlk(PyObject *module, PyObject *const *args, Py_ssize_t nargs)
+{
+    PyObject *return_value = NULL;
+    int fd;
+    int code;
+    PyObject *lenobj = NULL;
+    PyObject *startobj = NULL;
+    int whence = 0;
+
+    if (!_PyArg_CheckPositional("getlk", nargs, 2, 5)) {
+        goto exit;
+    }
+    if (!_PyLong_FileDescriptor_Converter(args[0], &fd)) {
+        goto exit;
+    }
+    code = PyLong_AsInt(args[1]);
+    if (code == -1 && PyErr_Occurred()) {
+        goto exit;
+    }
+    if (nargs < 3) {
+        goto skip_optional;
+    }
+    lenobj = args[2];
+    if (nargs < 4) {
+        goto skip_optional;
+    }
+    startobj = args[3];
+    if (nargs < 5) {
+        goto skip_optional;
+    }
+    whence = PyLong_AsInt(args[4]);
+    if (whence == -1 && PyErr_Occurred()) {
+        goto exit;
+    }
+skip_optional:
+    return_value = fcntl_getlk_impl(module, fd, code, lenobj, startobj, whence);
+
+exit:
+    return return_value;
+}
+/*[clinic end generated code: output=cef80296c2a7a9e1 input=a9049054013a1b77]*/